7d39b13773f2a3c24e72d45eea037a4cfceaf556
rajubanda81@gmail.com <rajubanda81@gmail.com>
2025-11-10 11:38:13 +0530

    routes added for api

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::apiResource('categories', App\Http\Controllers\Api\CategoryController::class);
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/user', [App\Http\Controllers\Api\AuthController::class, 'me']);
+    Route::apiResource('categories', App\Http\Controllers\Api\CategoryController::class);
+});
